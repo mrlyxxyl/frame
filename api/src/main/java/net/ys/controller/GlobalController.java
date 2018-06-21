59123cb199905351c23baa8609a0d366aa7a186d
liyuexin <1290821453@qq.com>
2018-06-21 11:30:12 +0800

    update

--- api/src/main/java/net/ys/controller/GlobalController.java
+++ api/src/main/java/net/ys/controller/GlobalController.java
@@ -1,12 +1,10 @@
 package net.ys.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import net.ys.constant.GenResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Map;
 
@@ -15,15 +13,13 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping(value = "global")
-@Api(value = "global-api", description = "全局接口")
+@ApiIgnore
 public class GlobalController {
 
     /**
      * HTTP 400
      */
     @RequestMapping(value = "http_400", method = RequestMethod.POST, headers = "Accept=application/json")
-    @ResponseBody
-    @ApiOperation(httpMethod = "POST", response = Map.class, responseContainer = "Map", value = "内部错误重定向")
     public Map<String, Object> http400() {
         return GenResult.PARAMS_ERROR.genResult();
     }
