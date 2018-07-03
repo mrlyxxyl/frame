e632f49210e7772571a1f48e7a85134d9a1df4e0
liyuexin <1290821453@qq.com>
2018-07-03 15:00:53 +0800

    update

--- core/src/main/java/net/ys/utils/AesUtil.java
+++ core/src/main/java/net/ys/utils/AesUtil.java
@@ -15,6 +15,8 @@ public class AesUtil {
 
     private static final String IV_STRING = "6868688xy8686866";
     private static final String KEY = "8686866xy6868688";
+    private static final String TITLE_CONTENT = "AES";
+    private static final String INS = "AES/CBC/PKCS5Padding";
 
     /**
      * 加密
@@ -26,10 +28,10 @@ public class AesUtil {
         try {
             byte[] byteContent = content.getBytes(Glc.Code.U);
             byte[] enCodeFormat = KEY.getBytes(Glc.Code.U);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, "AES");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, TITLE_CONTENT);
             byte[] initParam = IV_STRING.getBytes(Glc.Code.U);
             IvParameterSpec ivParameterSpec = new IvParameterSpec(initParam);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance(INS);
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
             byte[] encryptedBytes = cipher.doFinal(byteContent);
             return new BASE64Encoder().encode(encryptedBytes);
@@ -49,10 +51,10 @@ public class AesUtil {
             BASE64Decoder decoder = new BASE64Decoder();
             byte[] encryptedBytes = decoder.decodeBuffer(content);
             byte[] enCodeFormat = KEY.getBytes(Glc.Code.U);
-            SecretKeySpec secretKey = new SecretKeySpec(enCodeFormat, "AES");
+            SecretKeySpec secretKey = new SecretKeySpec(enCodeFormat, TITLE_CONTENT);
             byte[] initParam = IV_STRING.getBytes(Glc.Code.U);
             IvParameterSpec ivParameterSpec = new IvParameterSpec(initParam);
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance(INS);
             cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
             byte[] result = cipher.doFinal(encryptedBytes);
             return new String(result, Glc.Code.U);
